bba690f004f00736284d2aed117e993107aebb28
Julpjer <123937474+Julpjer@users.noreply.github.com>
2023-01-30 15:46:18 +0100

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -12,13 +12,13 @@ def evauation_model(pred, y_val):
   return score_MSE, score_MAE, score_r2score
 
 
-data = pd.read_csv("Fish.csv")
-data_cleaned = data.drop("Weight", axis=1)
-y = data['Weight']
-x_train, x_test, y_train, y_test = train_test_split(data_cleaned, y, test_size=0.2, random_state=42)
+data = pd.read_csv(r'C:\Users\jbroek\Desktop\ACC22.csv', delimiter=",")
+data_cleaned = data.drop("sigma", axis=1)
+y = data['sigma']
+x_train, x_test, y_train, y_test = train_test_split(data_cleaned, y, test_size=0.1, random_state=42)
 label_encoder = LabelEncoder()
-x_train['Species'] = label_encoder.fit_transform(x_train['Species'].values)
-x_test['Species'] = label_encoder.transform(x_test['Species'].values)
+x_train['ps'] = label_encoder.fit_transform(x_train['ps'].values)
+x_test['ps'] = label_encoder.transform(x_test['ps'].values)
 #save label encoder classes
 np.save('classes.npy', label_encoder.classes_)
 
@@ -33,8 +33,8 @@ loaded_encoder = LabelEncoder()
 loaded_encoder.classes_ = np.load('classes.npy',allow_pickle=True)
 print(x_test.shape)
 input_species = loaded_encoder.transform(np.expand_dims("Parkki",-1))
-print(int(input_species))
-inputs = np.expand_dims([int(input_species),15,20,10,4,5],0)
+print(int(input_ps))
+inputs = np.expand_dims([int(input_ps),15,20,10,4,5],0)
 print(inputs.shape)
 prediction = best_xgboost_model.predict(inputs)
 print("final pred", np.squeeze(prediction,-1))
